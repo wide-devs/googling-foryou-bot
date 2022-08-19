24aaa4edeedff05c9c17aa852d9b3382e00b7f0f
beliboba <belibobka@gmail.com>
2022-08-19 21:22:43 +0300

    Create

---
+++ tg.py
@@ -0,0 +1,16 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from config import keywords, token
+
+bot = Bot(token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=keywords)
+async def google(message=types.Message):
+	if message.reply_to_message:
+		await message.answer(f"https://google.com/search?q={message.reply_to_message.text}")
+
+
+if __name__ == "__main__":
+	executor.start_polling(dp)
---
+++ vk.py
@@ -0,0 +1,14 @@
+from vkbottle.bot import Bot, Message
+from vkbottle.dispatch.rules.base import ReplyMessageRule
+from config import keywords, token
+
+bot = Bot(token)
+
+
+@bot.on.message(ReplyMessageRule(), text=["google", "гугл", "погугли", "загугли"])
+async def google(message: Message):
+	await message.answer(f"https://google.com/search?q={message.reply_to_message.text}")
+
+
+if __name__ == "__main__":
+	bot.run_polling()
