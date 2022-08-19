a4ba855961936c5e820c95aaf1746823719da44d
beliboba <belibobka@gmail.com>
2022-08-19 21:26:36 +0300

    keywords

--- vk.py
+++ vk.py
@@ -5,7 +5,7 @@ from config import keywords, vktoken
 bot = Bot(vktoken)
 
 
-@bot.on.message(ReplyMessageRule(), text=["google", "гугл", "погугли", "загугли"])
+@bot.on.message(ReplyMessageRule(), text=keywords)
 async def google(message: Message):
 	await message.answer(f"https://google.com/search?q={message.reply_to_message.text}")
 
